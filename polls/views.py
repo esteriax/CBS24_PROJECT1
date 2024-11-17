31f0f5e80963c154b5c74d6ea53cb15f3e20c6f1
esteriax <parikkaheta@gmail.com>
2024-11-17 17:17:21 +0200

    Fix views.py

--- polls/views.py
+++ polls/views.py
@@ -41,12 +41,8 @@ def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
-    except:
-        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
-    """"
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
-    
         return render(
             request,
             "polls/detail.html",
@@ -55,7 +51,6 @@ def vote(request, question_id):
                 "error_message": "You didn't select a choice.",
             },
         )
-        
     else:
         selected_choice.votes = F("votes") + 1
         selected_choice.save()
@@ -63,4 +58,3 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
-        """
